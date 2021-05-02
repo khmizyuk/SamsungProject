f80cef33df4f4687e9b35cda6ca7d79d43edb8a8
Сергей <sergey.khmizyuk@gmail.com>
2021-05-02 21:54:47 +0300

    Firebase implimentation

--- app/src/main/java/com/example/login1703/LogInFragment.java
+++ app/src/main/java/com/example/login1703/LogInFragment.java
@@ -6,10 +6,11 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.annotation.VisibleForTesting;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -17,13 +18,20 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LogInFragment extends Fragment {
 
     //private static final String CORRECT_EMAIL = "khmizyuk";
     //private static final String CORRECT_PASSWORD = "1234";
 
-    public LogInFragment() { }
+    private static final String TAG = "EmailPassword";
+    private FirebaseAuth mAuth;
+
+    String email;
+    String password;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -31,8 +39,8 @@ public class LogInFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_log_in_design, container, false);
 
         MaterialButton sign_in_button = view.findViewById(R.id.sign_in_button);
-        MaterialButton complete_sign_in_button = view.findViewById(R.id.complete_sign_in_button);
         MaterialButton log_in_button = view.findViewById(R.id.log_in_button);
+
         TextInputEditText emailEditText = view.findViewById(R.id.log_in_email_input);
         TextInputEditText passwordEditText = view.findViewById(R.id.log_in_password_input);
         TextInputLayout passwordTextInput = view.findViewById(R.id.log_in_password_text_input);
@@ -47,17 +55,104 @@ public class LogInFragment extends Fragment {
         log_in_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                email = emailEditText.getText().toString();
+                password = passwordEditText.getText().toString();
+                signIn(email, password);
+                /*
                 if (!isPasswordValid(emailEditText.getText().toString(), passwordEditText.getText().toString())) {
                     passwordTextInput.setError(getString(R.string.log_in_error_password));
                     passwordEditText.setText(null);
                 } else {
                     passwordTextInput.setError(null); // Clear the error
                     ((NavigationHost) getActivity()).navigateTo(new MainPageFragment(), false); // Navigate to the next Fragment
-                }
+                }*/
             }
         });
 
+        mAuth = FirebaseAuth.getInstance();
+
         return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            ((NavigationHost) getActivity()).navigateTo(new MainPageFragment(), false);
+        }
+    }
+
+    private void signIn(String email, String password) {
+        Log.d(TAG, "signIn:" + email);
+
+        if (!validateForm()) {
+            Toast.makeText(getContext(), "Authentication failed.",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        showProgressBar();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(requireActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            ((NavigationHost) getActivity()).navigateTo(new MainPageFragment(), false);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(getContext(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        if (!task.isSuccessful()) { }
+                        hideProgressBar();
+                    }
+                });
+    }
+
+    @VisibleForTesting
+    public ProgressBar mProgressBar;
+
+    public void setProgressBar(ProgressBar progressBar) {
+        mProgressBar = progressBar;
+    }
+
+    public void showProgressBar() {
+        if (mProgressBar != null) {
+            mProgressBar.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void hideProgressBar() {
+        if (mProgressBar != null) {
+            mProgressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private boolean validateForm() {
+        boolean valid = true;
+
+        if (TextUtils.isEmpty(email)) {
+            valid = false;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            valid = false;
+        }
+
+        return valid;
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        hideProgressBar();
     }
 
     private boolean isPasswordValid(@NonNull String checkEmail, @NonNull String checkPassword) {
--- app/src/main/java/com/example/login1703/SignInFragment.java
+++ app/src/main/java/com/example/login1703/SignInFragment.java
@@ -2,96 +2,165 @@ package com.example.login1703;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.VisibleForTesting;
 import androidx.fragment.app.Fragment;
 
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
-//import com.google.firebase.auth.AuthResult;
-//import com.google.firebase.auth.FirebaseAuth;
-//import com.google.firebase.database.DatabaseReference;
-//import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignInFragment extends Fragment {
 
-    //FirebaseAuth auth; // Авторизация
-    //FirebaseDatabase db; // Подключение к бд
-    // DatabaseReference users; // Работа с табличками внутри бд
+    private static final String TAG = "EmailPassword";
+    private FirebaseAuth mAuth;
 
-    public SignInFragment() {
-        // Required empty public constructor
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+    String email, password, firstName, lastName;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_sign_in, container, false);
+        View view = inflater.inflate(R.layout.fragment_sing_in_design, container, false);
 
-        final TextInputEditText emailEditText = view.findViewById(R.id.log_in_email_input);
-        final TextInputEditText passwordEditText = view.findViewById(R.id.log_in_password_input);
-        final TextInputEditText firstname = view.findViewById(R.id.sign_in_name_input);
-        final TextInputEditText lastname = view.findViewById(R.id.sign_in_last_name_input);
+        final TextInputEditText emailEditText = view.findViewById(R.id.sign_in_email_input);
+        final TextInputEditText passwordEditText = view.findViewById(R.id.sign_in_password_input);
+        final TextInputEditText firstnameEditText = view.findViewById(R.id.sign_in_name_input);
+        final TextInputEditText lastnameEditText = view.findViewById(R.id.sign_in_last_name_input);
 
-        /*auth = FirebaseAuth.getInstance(); // Запускаем авторизацию в бд
-        db = FirebaseDatabase.getInstance(); // Подключаемся непосредственно к бд
-        users = db.getReference("Users"); // Указываем с какой табличкой работаем
+        MaterialButton signInButton = view.findViewById(R.id.complete_sign_in_button);
 
-        auth.createUserWithEmailAndPassword(emailEditText.getText().toString(), passwordEditText.getText().toString())
-                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-                    @Override
-                    public void onSuccess(AuthResult authResult) {
-                        User user = new User();
-                        user.setEmail((emailEditText.getText().toString()));
-                        user.setPassword((passwordEditText.getText().toString()));
-                        user.setFirstname((firstname.getText().toString()));
-                        user.setLastname((lastname.getText().toString()));
-
-                        users.child(user.getEmail())
-                                .setValue(user)
-                                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                    @Override
-                                    public void onSuccess(Void aVoid) {
-                                        Snackbar.make(container, "Пользователь добавлен", Snackbar.LENGTH_LONG).show();
-                                    }
-                                });
-                    }
-                });
-
-        Button complete_sign_in_button = view.findViewById(R.id.complete_sign_in_button);
-        complete_sign_in_button.setOnClickListener(new View.OnClickListener() {
+        signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (TextUtils.isEmpty(emailEditText.getText().toString())) {
-                    return;
-                }
-                if (TextUtils.isEmpty(passwordEditText.getText().toString())) {
-                    return;
-                }
-                if (TextUtils.isEmpty(firstname.getText().toString())) {
-                    return;
-                }
-                if (TextUtils.isEmpty(lastname.getText().toString())) {
-                    return;
-                }
-                //((NavigationHost) getActivity()).navigateTo(new LogInFragment(), false);
-            }
-        });*/
 
+                email = emailEditText.getText().toString();
+                password = passwordEditText.getText().toString();
+                firstName = firstnameEditText.getText().toString();
+                lastName = lastnameEditText.getText().toString();
+
+                if (!validateForm()) {
+                    Toast.makeText(getContext(), "Not validate form.",
+                            Toast.LENGTH_SHORT).show();
+                    return;
+                } else
+                    createAccount(email, password);
+            }
+        });
+
+        mAuth = FirebaseAuth.getInstance();
 
         return view;
     }
+
+
+    private void createAccount(String email, String password) {
+        Log.d(TAG, "createAccount:" + email);
+
+        showProgressBar();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(requireActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "createUserWithEmail:success");
+                            //sendEmailVerification();
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(getContext(), "Your account is created.",
+                                    Toast.LENGTH_SHORT).show();
+                            ((NavigationHost) getActivity()).navigateTo(new MainPageFragment(), true);
+                        } else {
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(getContext(), "Authentication failed " + task.getException(),
+                                    Toast.LENGTH_SHORT).show();
+                            hideProgressBar();
+                        }
+
+                        hideProgressBar();
+                    }
+                });
+    }
+
+    private void sendEmailVerification() {
+
+        final FirebaseUser user = mAuth.getCurrentUser();
+        user.sendEmailVerification()
+                .addOnCompleteListener(requireActivity(), new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getContext(),
+                                    "Verification email sent to " + user.getEmail(),
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            Log.e(TAG, "sendEmailVerification", task.getException());
+                            Toast.makeText(getContext(),
+                                    "Failed to send verification email.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    @VisibleForTesting
+    public ProgressBar mProgressBar;
+
+    public void setProgressBar(ProgressBar progressBar) {
+        mProgressBar = progressBar;
+    }
+
+    public void showProgressBar() {
+        if (mProgressBar != null) {
+            mProgressBar.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void hideProgressBar() {
+        if (mProgressBar != null) {
+            mProgressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private boolean validateForm() {
+        boolean valid = true;
+
+        if (TextUtils.isEmpty(email)) {
+            valid = false;
+        }
+        if (TextUtils.isEmpty(password)) {
+            valid = false;
+        }
+        /*if (TextUtils.isEmpty(firstName)) {
+            valid = false;
+        }
+        if (TextUtils.isEmpty(lastName)) {
+            valid = false;
+        }*/
+
+        return valid;
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        hideProgressBar();
+    }
+
 }
